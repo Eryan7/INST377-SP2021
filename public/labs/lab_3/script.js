9186411889477b17f03b7dd38893c56e1210bb1f
Emmet Ryan <emmetryan7@gmail.com>
2021-02-10 16:30:04 -0500

    Added javascript and changed some tags

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,2 +1,22 @@
 /* Put your javascript in here */
 "use strict";
+
+let position = 0;
+const list = carousel.querySelector("ul");
+let move_count = 0;
+
+right.onclick = function () {
+  if (move_count > -0) {
+    position += 310;
+    list.style.marginLeft = position + "px";
+    move_count -= 1;
+  }
+};
+
+left.onclick = function () {
+  if (move_count < 1) {
+    position -= 310;
+    list.style.marginLeft = position + "px";
+    move_count += 1;
+  }
+};
