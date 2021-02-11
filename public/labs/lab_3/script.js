a64f2752e3bbffcdc18719338755ef0198c542f0
Emmet Ryan <emmetryan7@gmail.com>
2021-02-11 10:06:20 -0500

    Updated to arrow funcs

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -3,26 +3,24 @@
 let position = 0;
 const list = carousel.querySelector("ul");
 
-hide_arrow(".right");
-
-right.onclick = function () {
+right.onclick = () => {
   position += 310;
   list.style.marginLeft = position + "px";
   hide_arrow(".right");
   unhide_arrow(".left");
 };
 
-left.onclick = function () {
+left.onclick = () => {
   position -= 310;
   list.style.marginLeft = position + "px";
   unhide_arrow(".right");
   hide_arrow(".left");
 };
 
-function hide_arrow(arrow) {
+let hide_arrow = (arrow) => {
   carousel.querySelector(arrow).style.visibility = "hidden";
 };
 
-function unhide_arrow(arrow){
+let unhide_arrow = (arrow) => {
   carousel.querySelector(arrow).style.visibility = "visible";
 };
